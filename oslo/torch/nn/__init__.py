881a5a06efb1c3d1e9e7be7c11cbe6c9f438d070
hyunwoongko <gusdnd852@naver.com>
2022-04-03 15:51:49 +0900

    Make fused bias functions accessible

---
+++ oslo/torch/nn/__init__.py
@@ -0,0 +1,6 @@
+from oslo.torch.nn.modules.fused_gelu import fused_gelu
+from oslo.torch.nn.modules.fused_bias_gelu import fused_bias_gelu
+from oslo.torch.nn.modules.fused_bias_dropout import (
+    fused_bias_dropout,
+    fused_bias_dropout_residual,
+)
