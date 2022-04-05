24b8ac71b0ad326a670376e8f2f16a58cdea574d
Ubuntu <ubuntu@ip-172-31-201-36.ec2.internal>
2022-04-05 01:16:42 +0000

    remove forward and load_state_dict pre hook when initialization

--- oslo/torch/nn/modules/lazy.py
+++ oslo/torch/nn/modules/lazy.py
@@ -6,3 +6,9 @@ class ParentLazyModuleMixin(LazyModuleMixin):
         for module in self.modules():
             if isinstance(module, LazyModuleMixin):
                 module.initialize_parameters(*args, **kwargs)
+            if hasattr(module, "_initialize_hook"):
+                module._initialize_hook.remove()
+                delattr(module, '_initialize_hook')
+            if hasattr(module, "_load_hook"):
+                module._load_hook.remove()
+                delattr(module, '_load_hook')
