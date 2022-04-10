95ac51700d59069c2fddb5da384c00c5c36f280e
bzantium <ryumin93@gmail.com>
2022-04-10 12:04:16 +0000

    Add basic distributed ops and Lazy Linear modules

--- oslo/torch/nn/modules/lazy.py
+++ oslo/torch/nn/modules/lazy.py
@@ -1,16 +1,32 @@
-from torch.nn.modules.lazy import LazyModuleMixin
+import itertools
+from torch.nn.parameter import is_lazy
+from torch.nn.modules.lazy import _LazyProtocol
 
 
-class ParentLazyModuleMixin(LazyModuleMixin):
-    def initialize_parameters(self, *args, **kwargs):
-        for module in self.modules():
-            if isinstance(module, LazyModuleMixin):
-                module.initialize_parameters(*args, **kwargs)
-                if hasattr(module, "_initialize_hook"):
-                    module._initialize_hook.remove()
-                    delattr(module, '_initialize_hook')
-                if hasattr(module, "_load_hook"):
-                    module._load_hook.remove()
-                    delattr(module, '_load_hook')
-                if module.cls_to_become is not None:
-                    module.__class__ = module.cls_to_become
+class LazyModuleMixin:
+    cls_to_become = None
+
+    def __init__(self: _LazyProtocol, *args, **kwargs):
+        # Mypy doesnt like this super call in a mixin
+        super().__init__(*args, **kwargs)  # type: ignore[misc]
+
+    def has_uninitialized_params(self: _LazyProtocol):
+        r"""Check if a module has parameters that are not initialized
+        """
+        # This is to avoid the JIT to track this parameter and force
+        # custom modules __setstate__ to add it
+        params = self._parameters.values()
+        buffers = self._buffers.values()
+        for param in itertools.chain(params, buffers):
+            if is_lazy(param):
+                return True
+        return False
+
+    def initialize_parameters(self: _LazyProtocol, *args, **kwargs):
+        r"""Initialize parameters according to the input batch properties.
+        This adds an interface to isolate parameter initialization from the
+        forward pass when doing parameter shape inference.
+        """
+        raise NotImplementedError(
+            'initialize_parameters is not implemented for {}'.format(self.__class__.__name__)
+        )
--- oslo/torch/nn/modules/linear.py
+++ oslo/torch/nn/modules/linear.py
@@ -1,12 +1,63 @@
-from typing import Union, Tuple
+from typing import Union, Tuple, Optional
+import math
+
+from oslo.torch.distributed import ParallelMode
 
 import torch
 import torch.nn as nn
+from torch.nn.parameter import Parameter, UninitializedParameter
 import torch.nn.functional as F
-from torch.nn.modules.lazy import LazyModuleMixin
+
+from .lazy import LazyModuleMixin
+from oslo.torch.utils import divide
 
 
-class LazyLinear(LazyModuleMixin, nn.Linear):
+class Linear(nn.Module):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        bias: bool = True,
+        device: Optional[torch.device] = None,
+        dtype: Optional[torch.dtype] = None,
+        skip_bias_add: bool =False,
+    ):
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        super(Linear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.empty((out_features, in_features), **factory_kwargs))
+        if bias:
+            self.bias = Parameter(torch.empty(out_features, **factory_kwargs))
+        else:
+            self.register_parameter('bias', None)
+        self.skip_bias_add = skip_bias_add
+
+    def reset_parameters(self) -> None:
+        # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
+        # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
+        # https://github.com/pytorch/pytorch/issues/57109
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(
+        self, input: torch.Tensor
+    ) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+        if not self.skip_bias_add:
+            return F.linear(input, self.weight, self.bias)
+        else:
+            return F.linear(input, self.weight), self.bias
+
+    def extra_repr(self) -> str:
+        return 'in_features={}, out_features={}, bias={}'.format(
+            self.in_features, self.out_features, self.bias is not None
+        )
+    
+
+class LazyLinear(LazyModuleMixin, Linear):
     """
     Lazy initialized linear layer.
 
@@ -44,18 +95,18 @@ class LazyLinear(LazyModuleMixin, nn.Linear):
                 [ 0.4704,  0.6281]], requires_grad=True)
     """
 
-    cls_to_become = nn.Linear
-    weight: nn.UninitializedParameter
-    bias: nn.UninitializedParameter
+    cls_to_become = Linear
+    weight: UninitializedParameter
+    bias: UninitializedParameter
 
     def __init__(
         self,
         in_features: int,
         out_features: int,
         bias: bool = True,
-        device=None,
-        dtype=None,
-        skip_bias_add=False,
+        device: Optional[torch.device] = None,
+        dtype: Optional[torch.dtype] = None,
+        skip_bias_add: bool =False,
     ) -> None:
         factory_kwargs = {"device": device, "dtype": dtype}
         super().__init__(0, 0, False)
@@ -63,20 +114,12 @@ class LazyLinear(LazyModuleMixin, nn.Linear):
         self.out_features = out_features
         self.skip_bias_add = skip_bias_add
 
-        self.weight = nn.UninitializedParameter(**factory_kwargs)
+        self.weight = UninitializedParameter(**factory_kwargs)
         if bias:
-            self.bias = nn.UninitializedParameter(**factory_kwargs)
-
-    def forward(
-        self, input: torch.Tensor
-    ) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
-        if not self.skip_bias_add:
-            return F.linear(input, self.weight, self.bias)
-        else:
-            return F.linear(input, self.weight), self.bias
+            self.bias = UninitializedParameter(**factory_kwargs)
 
     def reset_parameters(self) -> None:
-        if not self.has_uninitialized_params():
+        if not self.has_uninitialized_params() and self.in_features != 0:
             super().reset_parameters()
 
     def initialize_parameters(self) -> None:
@@ -87,15 +130,5 @@ class LazyLinear(LazyModuleMixin, nn.Linear):
                 if self.bias is not None:
                     self.bias.materialize((self.out_features,))
                 self.reset_parameters()
-
-
-class ColumnParallelLinear(nn.Module):
-    pass
-
-
-class RowParallelLinear(nn.Module):
-    pass
-
-
-class Linear2D(nn.Module):
-    pass
+        if self.cls_to_become is not None:
+            self.__class__ = self.cls_to_become
