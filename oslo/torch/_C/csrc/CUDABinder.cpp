6f170d54dda733c8c7f7ae4455adaab6e761f101
Granzou <loopinf@gmail.com>
2022-04-10 11:05:19 +0000

    refactor: Remove unused and redundant parts
    ngram_repeat_block_cuda_forward is removed.

--- oslo/torch/_C/csrc/CUDABinder.cpp
+++ oslo/torch/_C/csrc/CUDABinder.cpp
@@ -3,12 +3,8 @@
 #include <torch/torch.h>
 #include <vector>
 #include <cassert>
-#include <stdio.h>
+#include "compat.h"
 
-torch::Tensor ngram_repeat_block_cuda_forward(torch::Tensor tokens,
-        torch::Tensor lprobs, int bsz,
-        int step, int beam_size,
-        int no_repeat_ngram_size);
 
 #define CHECK_CUDA(x) \
   TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
@@ -20,26 +16,6 @@ torch::Tensor ngram_repeat_block_cuda_forward(torch::Tensor tokens,
 
 // Input check and call to CUDA OP
 // Backward method not required
-torch::Tensor ngram_repeat_block_forward(torch::Tensor tokens,
-        torch::Tensor lprobs, int bsz,
-        int step, int beam_size,
-        int no_repeat_ngram_size) {
-    CHECK_INPUT(tokens);
-    CHECK_INPUT(lprobs);
-    assert(bsz > 0);
-    assert(step >= 0);
-    assert(beam_size > 0);
-    assert(no_repeat_ngram_size > 0);
-
-    return ngram_repeat_block_cuda_forward(tokens, lprobs, bsz, step, beam_size,
-                                           no_repeat_ngram_size);
-}
-
-#include <torch/extension.h>
-#include <vector>
-#include <cassert>
-#include "compat.h"
-
 namespace {
 void compute_n1_n2(
     at::Tensor input,
