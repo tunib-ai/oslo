8c69160271ba9cd0b59248172d7c2d1cea7a960b
gimmaru <john8778@naver.com>
2022-04-13 00:19:17 +0900

    add ProcessorForCausalLM refer to lassl

--- oslo/transformers/tasks/data_causal_lm.py
+++ oslo/transformers/tasks/data_causal_lm.py
@@ -1,15 +1,66 @@
-from typing import Any, Dict, List, Optional
+from abc import ABC, abstractmethod
+from typing import Dict, List, Optional
 
+from transformers import AutoTokenizer
 from transformers.data.data_collator import _torch_collate_batch
 from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 
 
-# def tolist(x):
-#     if isinstance(x, list):
-#         return x
-#     elif hasattr(x, "numpy"): # Checks for TF tensors without needing the import
-#         x = x.numpy()
-#     return x.tolist()
+class BaseProcessor(ABC):
+    def __init__(self, model_name_or_path: str, max_length: int) -> None:
+        self._tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+        self._max_length = max_length
+        self._chunk_size = max_length
+        self._buffer = []
+
+    def save_tokenizer(self, path: str) -> None:
+        self._tokenizer.save_pretrained(path)
+
+    @abstractmethod
+    def __call__(self, list_of_str: List[str]) -> Dict[str, List[int]]:
+        pass
+
+
+class ProcessorForCausalLM(BaseProcessor):
+    def __init__(self, model_name_or_path: str, max_length: int) -> None:
+        super().__init__(model_name_or_path=model_name_or_path, max_length=max_length)
+        self._chunk_size = max_length
+    
+    def __call__(self, list_of_str: List[str]) -> Dict[str, List[int]]:
+        dict_of_training_examples: Dict[str, List[int]] = {}
+
+        list_of_input_ids: List[List[int]] = self._tokenizer(
+            list_of_str,
+            padding=False,
+            truncation=False,
+            return_attention_mask=False,
+            return_special_tokens_mask=False,
+            verbose=False,
+        )["input_ids"]
+
+        for input_ids in list_of_input_ids:
+            input_ids += [self._tokenizer.eos_token_id]
+            self._buffer.extend(input_ids)
+
+            while len(self._buffer) >= self._chunk_size:
+                chunk_ids = self._buffer[: self._chunk_size]
+                training_example = self._tokenizer.prepare_for_model(
+                    chunk_ids,
+                    padding=False,
+                    truncation=False,
+                    return_attention_mask=False,
+                    return_token_type_ids=False,
+                    add_special_tokens=False,
+                )
+
+                for key in training_example.keys():
+                    if key not in dict_of_training_examples:
+                        dict_of_training_examples.setdefault(key, [])
+                    dict_of_training_examples[key].append(training_example[key])
+                
+                self._buffer = self._buffer[self._chunk_size :]
+        
+        return dict_of_training_examples
 
 
 class DataCollatorForCausalLM:
@@ -22,16 +73,15 @@ class DataCollatorForCausalLM:
         tokenizer: PreTrainedTokenizerBase,
         pad_to_multiple_of: Optional[int] = None,
     ):
-        self.tokenzier = tokenizer
+        self.tokenizer = tokenizer
         self.pad_to_multiple_of = pad_to_multiple_of
-    
-    def __call__(self, example):
+
+    def __call__(self, examples):
         examples = [example["input_ids"] for example in examples]
         batch = {
             "input_ids": _torch_collate_batch(
-                example, tokenizer=self.tokenizer, pad_to_multiple_of=self.pad_to_multiple_of
+                examples, tokenizer=self.tokenizer, pad_to_multiple_of=self.pad_to_multiple_of
             )
         }
         batch["labels"] = batch["input_ids"].clone()
         return batch
-
