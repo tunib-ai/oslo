1efea9be8fb0d4df2d994128fd140298be44f37c
Taekyun Kim <brandon1609@naver.com>
2022-04-16 08:45:39 +0000

    Add FusedScaleMaskSoftmax python wrapper

--- oslo/torch/nn/__init__.py
+++ oslo/torch/nn/__init__.py
@@ -10,3 +10,10 @@ from oslo.torch.nn.modules.dropout import (
 )
 from oslo.torch.nn.modules.linear import LazyLinear
 from oslo.torch.nn.modules.conv import Conv1D, LazyConv1D
+from oslo.torch.nn.modules.softmax import FusedScaleMaskSoftmax
+# from oslo.torch.nn.modules.enums import (
+#     ModelType,
+#     LayerType,
+#     AttnType,
+#     AttnMaskType,
+# )
--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-from oslo.torch._C import CUDABinder
+from oslo.torch._C import SoftmaxBinder
+from oslo.torch import nn
 
+# CUDA = SoftmaxBinder().bind()
+# print(CUDA.scaled_masked_softmax_forward)
+# print(CUDA.scaled_masked_softmax_backward)
+# print(CUDA.get_batch_per_block)
+# print(CUDA.scaled_upper_triang_masked_softmax_forward)
+# print(CUDA.scaled_upper_triang_masked_softmax_backward)
 
-CUDABinder().bind()
+# print(nn.Conv1D)
+print(nn.FusedScaleMaskSoftmax)
