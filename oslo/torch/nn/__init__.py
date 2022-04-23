c21b0cecb4708abba14df8b633e12b4d703cf0fb
granzou <loopinf@gmail.com>
2022-04-23 19:45:50 +0900

    refactor: Add function on onn module

--- oslo/torch/nn/__init__.py
+++ oslo/torch/nn/__init__.py
@@ -3,6 +3,12 @@ from oslo.torch.nn.modules.functional import (
     fused_bias_gelu,
     fused_bias_dropout,
     fused_bias_dropout_residual,
+    fused_rms_norm_affine,
+    fused_layer_norm,
+    mixed_dtype_fused_layer_norm_affine,
+    fused_layer_norm_affine,
+    mixed_dtype_fused_rms_norm_affine,
+    fused_rms_norm,
 )
 from oslo.torch.nn.modules.dropout import (
     FusedBiasDropout,
@@ -11,15 +17,15 @@ from oslo.torch.nn.modules.dropout import (
 from oslo.torch.nn.modules.linear import LazyLinear
 from oslo.torch.nn.modules.conv import Conv1D, LazyConv1D
 
-from oslo.torch.nn.modules.fused_layer_norm import (
-    FusedLayerNorm,
-    MixedFusedLayerNorm,
-    MixedFusedRMSNorm,
-    FusedRMSNorm,
-    fused_rms_norm_affine,
-    fused_layer_norm,
-    mixed_dtype_fused_layer_norm_affine,
-    fused_layer_norm_affine,
-    mixed_dtype_fused_rms_norm_affine,
-    fused_rms_norm,
-)
+# from oslo.torch.nn.modules.fused_layer_norm import (
+#     FusedLayerNorm,
+#     MixedFusedLayerNorm,
+#     MixedFusedRMSNorm,
+#     FusedRMSNorm,
+#     fused_rms_norm_affine,
+#     fused_layer_norm,
+#     mixed_dtype_fused_layer_norm_affine,
+#     fused_layer_norm_affine,
+#     mixed_dtype_fused_rms_norm_affine,
+#     fused_rms_norm,
+# )
