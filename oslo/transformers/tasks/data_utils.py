1fe20323492d1d77c53a7b944597d46bfc231eee
gimmaru <john8778@naver.com>
2022-04-27 21:27:17 +0900

    add train_tokenizer and serialize_corpora function

--- oslo/transformers/tasks/data_utils.py
+++ oslo/transformers/tasks/data_utils.py
@@ -2,14 +2,24 @@ from pathlib import Path
 from typing import Generator, Union
 
 import datasets
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk, Dataset, DatasetDict
 from transformers import AutoTokenizer
+from transformers.file_utils import ExplicitEnum
 
 from abc import ABC, abstractmethod
 from typing import Dict, List, Optional
+from data_causal_lm import ProcessorForCausalLM
+from data_sequence_classification import ProcessorForSequenceClassification
+from data_token_classification import ProcessorForTokenClassification
 
 SENT_TEXT_SCRIPT = str((Path(__file__).parent / "loading" / "sent_text.py").resolve().absolute())
 
+class CorpusType(ExplicitEnum):
+    DOCU_TEXT = "docu_text"
+    DOCU_JSON = "docu_json"
+    SENT_TEXT = "sent_text"
+    SENT_JSON = "sent_json"
+    DATASET = "dataset"
 
 def batch_iterator(
     dataset: datasets.arrow_dataset.Dataset,
@@ -20,7 +30,7 @@ def batch_iterator(
             for i in range(0, len(dataset), batch_size))
 
 
-def load_corpora(dir_path, corpus_type="docu_json"):
+def load_corpora(dir_path: str, corpus_type: str = "docu_json") -> Union[Dataset, DatasetDict]:
     corpora_dir = Path(dir_path).absolute()
     print(corpora_dir)
     extension = corpus_type.split("_")[-1]
@@ -48,16 +58,112 @@ def load_corpora(dir_path, corpus_type="docu_json"):
         raise ValueError(f"{corpus_type} must be one of ['docu_text', 'docu_json', 'sent_text', 'sent_json']")
 
 
-class BaseProcessor(ABC):
-    def __init__(self, model_name_or_path: str, max_length: int) -> None:
-        self._tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-        self._max_length = max_length
-        self._chunk_size = max_length
-        self._buffer = []
+def train_tokenizer(
+    model_type: str = "gpt2",
+    vocab_size: int = 30000,
+    min_frequency: int = 2,
+    additional_special_tokens: Optional[List[str]] = None,
+    corpora_dir: str = "./corpora",
+    corpus_type: CorpusType = "docu_text",
+    batch_size: int = 1000,
+    sampling_ratio: float = 0.1,
+):
+    from numpy.random import choice
 
-    def save_tokenizer(self, path: str) -> None:
-        self._tokenizer.save_pretrained(path)
+    model_type_to_predefined_model = {
+        "gpt2": "gpt2"
+    }
 
-    @abstractmethod
-    def __call__(self, list_of_str: List[str]) -> Dict[str, List[int]]:
-        pass
+    if corpus_type == "dataset":
+        corpora = load_from_disk(corpora_dir)["train"]
+    else:
+        corpora = load_corpora(corpora_dir, corpus_type)
+
+    assert sampling_ratio > 0, "Sampling_ratio must be greater than 0."
+
+    if 0 < sampling_ratio < 1.0:
+        total_size = len(corpora)
+        sample_size = int(total_size * sampling_ratio)
+        corpora = corpora.select(indices=choice(total_size, sample_size, replace=False))
+    else:
+        print("Since sampling_ratio >= 1.0, all corpora will be used.")
+    
+    tokenizer = AutoTokenizer.from_pretrained(model_type_to_predefined_model[model_type])
+    data_iterator = batch_iterator(corpora, batch_size=batch_size)
+
+    if additional_special_tokens:
+        assert len(additional_special_tokens) == len(
+            set(additional_special_tokens)
+        ), "Each additional special tokens must be unique."
+        assert not set(tokenizer.all_special_tokens).intersection(
+            set(additional_special_tokens)
+        ), "Each additional special tokens are not of default special tokens from tokenizer."
+
+        tokenizer = tokenizer.train_new_from_iterator(
+            data_iterator,
+            vocab_size=vocab_size,
+            min_frequency=min_frequency,
+            new_special_tokens=additional_special_tokens,
+        )
+    else:
+        tokenizer = tokenizer.train_new_from_iterator(
+            data_iterator,
+            vocab_size=vocab_size,
+            min_frequency=min_frequency,
+        )
+
+    corpora_name = corpora_dir.split("/")[-1]
+    tokenizer.save_pretrained(f"tokenizers/{model_type}_{corpora_name}")
+
+
+def serialize_corpora(
+    model_type: str = "gpt2",
+    task_type: str = "causal_lm",
+    tokenizer_dir: str = "./tokenizers/gpt2",
+    corpora_dir: str = "./corpora",
+    corpus_type: CorpusType = "docu_text",
+    max_length: int = 512,
+    batched: bool = True,
+    num_proc: Optional[int] = None,
+    batch_size: int = 1000,
+    writer_batch_size: int = 1000,
+    load_from_cache_file: bool = True,
+    keep_in_memory: bool = False,
+):
+    task_type_to_processor = {
+        "causal_lm": ProcessorForCausalLM,
+        "sequence_classification": ProcessorForSequenceClassification,
+        "token_classification": ProcessorForTokenClassification,
+    }
+
+    assert task_type in task_type_to_processor, "task_type must be in {'causal_lm', 'sequence_classification', 'token_classification'}"
+
+    data_processor = task_type_to_processor[task_type](tokenizer_dir, max_length)
+
+    if corpus_type == "dataset":
+        corpora = load_from_disk(corpora_dir)["train"]
+    else:
+        corpora = load_corpora(corpora_dir, corpus_type)
+
+    if "label" in corpora.column_names:
+        corpora = corpora.rename_column("label", "labels")
+    
+    if "label_ids" in corpora.column_names:
+        corpora = corpora.rename_column("label_ids", "labels")
+        
+    if task_type == "token_classification":
+        data_processor.get_label_names(corpora)
+    
+    dataset = corpora.map(
+        lambda examples: data_processor(examples["text"]) if task_type == "causal_lm" else data_processor(examples),
+        batched=batched,
+        num_proc=num_proc,
+        batch_size=batch_size,
+        writer_batch_size=writer_batch_size,
+        load_from_cache_file=load_from_cache_file,
+        keep_in_memory=keep_in_memory,
+        remove_columns=corpora.column_names,
+    )
+
+    dataset.save_to_disk(f"datasets/{model_type}_{task_type}")
+    data_processor.save_tokenizer(f"datasets/{model_type}_{task_type}")
