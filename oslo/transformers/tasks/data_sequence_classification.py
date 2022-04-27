dd6641e3b61b541593ad553490765f6dc709f728
gimmaru <john8778@naver.com>
2022-04-27 21:30:08 +0900

    add BaseProcessor to resolve circular import error

--- oslo/transformers/tasks/data_sequence_classification.py
+++ oslo/transformers/tasks/data_sequence_classification.py
@@ -1,19 +1,36 @@
 from typing import Any, Dict, List, Optional
+from abc import ABC, abstractmethod
 
-from transformers import GPT2ForSequenceClassification
+from transformers import GPT2ForSequenceClassification, AutoTokenizer
 from transformers.file_utils import PaddingStrategy
-from data_utils import BaseProcessor
+from datasets.arrow_dataset import Batch
+
+
+class BaseProcessor(ABC):
+    def __init__(self, model_name_or_path: str, max_length: int) -> None:
+        self._tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+        self._max_length = max_length
+        self._chunk_size = max_length
+        self._buffer = []
+
+    def save_tokenizer(self, path: str) -> None:
+        self._tokenizer.save_pretrained(path)
+
+    @abstractmethod
+    def __call__(self, list_of_str: List[str]) -> Dict[str, List[int]]:
+        pass
 
 
 class ProcessorForSequenceClassification(BaseProcessor):
     def __init__(self, model_name_or_path: str, max_length: Optional[int] = None) -> None:
         super().__init__(model_name_or_path=model_name_or_path, max_length=max_length)
     
-    def __call__(self, list_of_str: List[str]) -> Dict[str, List[int]]:
+    def __call__(self, examples: Batch) -> Dict[str, List[int]]:
         dict_of_training_examples: Dict[str, List[int]] = self._tokenizer(
-            list_of_str,
+            examples["text"],
             verbose=False,
         )
+        dict_of_training_examples["labels"] = examples["labels"]
 
         return dict_of_training_examples
 
