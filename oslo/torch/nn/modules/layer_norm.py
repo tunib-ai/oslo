eab7cd01101c0be5b4c46a9c863cc465584dc983
jason960903 <ykcha9@gmail.com>
2022-04-30 14:56:28 +0900

    implemented 2p5d layernorm

--- oslo/torch/nn/modules/layer_norm.py
+++ oslo/torch/nn/modules/layer_norm.py
@@ -1,3 +1,5 @@
+from typing import Union, Tuple, Optional
+
 import torch
 import torch.distributed as dist
 import torch.nn as nn
@@ -7,6 +9,10 @@ from oslo.torch.distributed import ParallelContext, ParallelMode
 from oslo.torch.nn.parallel.tensor_parallel._parallel_2d._ops import (
     add_bias_2d,
     layernorm_2d,
+)
+from oslo.torch.nn.parallel.distributed.tensor_parallel.parallel_2p5d._ops import (
+    layernorm_2p5d,
+    add_bias_2p5d,
 )
 
 
@@ -116,6 +122,72 @@ class LayerNorm2D(nn.Module):
                 self.pipeline_parallel_size,
                 self.tensor_parallel_size,
             )
+            output = torch.addcmul(bias, scale, output)
+        else:
+            output = torch.mul(scale, output)
+        return output
+
+
+class LayerNorm2p5D(nn.Module):
+    def __init__(
+            self,
+            normalized_shape: int,
+            parallel_context: ParallelContext,
+            eps: float = 1e-05,
+            bias: bool = True,
+            device: Optional[torch.device] = None,
+            dtype=None
+    ):
+        super().__init__()
+        self.parallel_context = parallel_context
+        self.tesseract_dim = self.parallel_context.get_world_size(ParallelMode.TENSOR_2P5D_COL)
+        assert (
+                normalized_shape % self.summa_dim == 0
+        ), "normalized_shape must be divisible by tessract dim."
+
+        self.normalized_shape = normalized_shape
+        self.variance_epsilon = eps
+        self.bias = bias
+
+        self.row_rank = self.parallel_context.get_local_rank(ParallelMode.TENSOR_2P5D_COL)
+        self.col_rank = self.parallel_context.get_local_rank(ParallelMode.TENSOR_2P5D_ROW)
+        self.data_parallel_rank = self.parallel_context.get_local_rank(ParallelMode.DATA)
+        self.pipeline_parallel_rank = self.parallel_context.get_local_rank(ParallelMode.PIPELINE)
+
+        self.tensor_parallel_size = self.parallel_context.get_world_size(ParallelMode.TENSOR)
+        self.pipeline_parallel_size = self.parallel_context.get_world_size(ParallelMode.PIPELINE)
+
+        self.partitioned_dim = normalized_shape // self.tesseract_dim
+        factory_kwargs = {'device': device, 'dtype': dtype}
+
+        self.gamma = Parameter(torch.ones(self.normalized_shape, **factory_kwargs))
+        self.beta = Parameter(torch.zeros(self.normalized_shape, **factory_kwargs))
+
+    def forward(self, _input: torch.Tensor) -> torch.Tensor:
+        with torch.no_grad():
+            E_x = torch.sum(_input, dim=-1, keepdim=True)  # [b/q, s, 1]
+            torch.distributed.all_reduce(E_x, group=self.parallel_context.get_group(ParallelMode.TENSOR_2P5D_ROW))
+            E_x /= self.normalized_shape
+
+            # Var_x in the block below is the sum of input^2
+            Var_x = torch.sum(_input * _input, dim=-1, keepdim=True)  # [b/q, s, 1]
+            torch.distributed.all_reduce(Var_x, group=self.parallel_context.get_group(ParallelMode.TENSOR_2P5D_ROW))
+            Var_x /= self.normalized_shape
+
+            Var_x = Var_x - E_x * E_x  # variance of x [b/q, s, 1]
+            # this time 1/sqrt(Var_x + epsilon)
+            Var_x = 1.0 / torch.sqrt(Var_x + self.variance_epsilon)
+
+        output = layernorm_2p5d(_input, E_x, Var_x, self.normalized_shape, ParallelMode.TENSOR_2P5D_ROW)
+        scale = add_bias_2p5d(None, self.gamma, self.partitioned_dim, self.tesseract_dim, self.row_rank,
+                              self.col_rank, self.dep_rank, ParallelMode.TENSOR_2P5D_COL, True,
+                              self.data_parallel_rank, self.pipeline_parallel_rank, self.pipeline_parallel_size,
+                              self.tensor_parallel_size)
+        if self.bias is not None:
+            bias = add_bias_2p5d(None, self.beta, self.partitioned_dim, self.tesseract_dim, self.row_rank,
+                                 self.col_rank, self.dep_rank, ParallelMode.TENSOR_2P5D_COL, True,
+                                 self.data_parallel_rank, self.pipeline_parallel_rank, self.pipeline_parallel_size,
+                                 self.tensor_parallel_size)
             output = torch.addcmul(bias, scale, output)
         else:
             output = torch.mul(scale, output)
