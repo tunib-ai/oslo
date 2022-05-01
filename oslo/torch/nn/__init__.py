cb612e831e391a96e1c7d09ad5e4e67b87562cb7
granzou <loopinf@gmail.com>
2022-05-01 14:34:21 +0000

    refactor: Reorganize from previous branch

--- oslo/torch/nn/__init__.py
+++ oslo/torch/nn/__init__.py
@@ -27,3 +27,22 @@ from oslo.torch.nn.modules.linear import (
     Linear2p5D,
     RowParallelLinear,
 )
+from oslo.torch.nn.modules.functional import (
+    fused_gelu,
+    fused_bias_gelu,
+    fused_bias_dropout,
+    fused_bias_dropout_residual,
+    fused_rms_norm_affine,
+    fused_layer_norm,
+    mixed_dtype_fused_layer_norm_affine,
+    fused_layer_norm_affine,
+    mixed_dtype_fused_rms_norm_affine,
+    fused_rms_norm,
+)
+
+from oslo.torch.nn.modules.fused_layer_norm import (
+    FusedLayerNorm,
+    MixedFusedLayerNorm,
+    MixedFusedRMSNorm,
+    FusedRMSNorm,
+)
