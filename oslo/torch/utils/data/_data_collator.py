8a6481f85767973ed95414828f6f0e98edc9c0e3
JungHyun Cho <reniew2@gmail.com>
2022-05-01 20:38:04 +0900

    data collator wrapper

--- oslo/torch/utils/data/_data_collator.py
+++ oslo/torch/utils/data/_data_collator.py
@@ -1,31 +1,40 @@
 import torch
+import math
+from torch.utils.data import default_collate
 from oslo.torch.distributed import ParallelContext, ParallelMode
+from typing import Optional, Callable, List
 
+class SequenceParallelDataCollatorWrapper:
+    def __init__(
+        self,
+        parallel_context: ParallelContext,
+        parallelize_keys: List[str],
+        batch_first: bool = True,
+        pad_token_id: Optional[int] = 0,
+        collate_fn: Optional[Callable] = None
+    ):
+        self.parallelize_keys = parallelize_keys
+        self.batch_index = 0 if batch_first else 1
+        self.seq_index = 1 if batch_first else 0
+        self.pad_token_id = pad_token_id
+        self.local_rank = parallel_context.get_local_rank(ParallelMode.SEQUENCE)
+        self.local_world_size = parallel_context.get_world_size(ParallelMode.SEQUENCE)
+        self.collate_fn = collate_fn if collate_fn is not None else default_collate
 
-class SequenceParallelDataCollator:
-    def __init__(self, gpc: ParallelContext):
-        self.local_rank = gpc.get_local_rank(ParallelMode.SEQUENCE)
-        self.local_world_size = gpc.get_world_size(ParallelMode.SEQUENCE)
+    def __call__(self, **kwargs):
 
-    def __call__(self, features):
-        if not isinstance(features[0], dict):
-            features = [vars(f) for f in features]
-        first = features[0]
-        batch = {}
+        features = self.collate_fn(**kwargs)
+        for key in self.parallelize_keys:
+            value = features[key]
+            batch_size = value.size(self.batch_index)
+            seq_length = value.size(self.seq_index)
+            remainder = seq_length % self.local_world_size
+            if remainder > 0:
+                pads = torch.ones(size=(batch_size, remainder), dtype=value.dtype, device=value.device)
+                value = torch.hstack((value, pads))
+            sub_seq_length = math.ceil(seq_length / self.local_world_size)
+            start_idx = self.local_rank * sub_seq_length
+            end_idx = (self.local_rank + 1) * sub_seq_length
+            features[key] = value[:, start_idx:end_idx]
 
-        for k, v in first.items():
-            if v is not None and not isinstance(v, str):
-                seq_length = len(v)
-                assert seq_length % self.local_world_size == 0, \
-                    f'Sequence length must be multiply of world_size({self.local_world_size})'
-                sub_seq_length = seq_length // self.local_world_size
-
-                start_idx = self.local_rank * sub_seq_length
-                end_idx = (self.local_rank + 1) * sub_seq_length
-                if isinstance(v, torch.Tensor):
-                    batch[k] = torch.stack([f[k][start_idx:end_idx] for f in features])
-                else:
-                    dtype = torch.long if isinstance(v, int) else torch.float
-                    batch[k] = torch.tensor([f[k][start_idx:end_idx] for f in features], dtype=dtype)
-
-        return batch
+        return features
