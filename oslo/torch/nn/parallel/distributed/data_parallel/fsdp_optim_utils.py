8effdf463cf457dee3e501c8913803990a63d5ce
hyunwoongko <gusdnd852@naver.com>
2022-05-01 20:30:21 +0900

    add ZeRO with TP test

--- oslo/torch/nn/parallel/distributed/data_parallel/fsdp_optim_utils.py
+++ oslo/torch/nn/parallel/distributed/data_parallel/fsdp_optim_utils.py
@@ -9,7 +9,6 @@ from typing import TYPE_CHECKING, Any, Dict, Iterator, List, Tuple, cast
 
 import torch
 
-from oslo.torch.utils import FlattenParamsWrapper
 
 if TYPE_CHECKING:
     from oslo.torch.nn.parallel.distributed.data_parallel import (
@@ -71,7 +70,9 @@ def flatten_optim_state_dict(sd: Dict) -> Dict:
     return new_sd
 
 
-def check_param_counts_before_sharding(full_optim_state_dict: Dict, n_instances: int) -> None:
+def check_param_counts_before_sharding(
+    full_optim_state_dict: Dict, n_instances: int
+) -> None:
     n_local_params_in_opt = len(set(full_optim_state_dict["param_id_map"].values()))
     msg = (
         f"Including itself, this model has {n_instances} nested instances. When the optimizer state was saved "
@@ -83,15 +84,21 @@ def check_param_counts_before_sharding(full_optim_state_dict: Dict, n_instances:
 
 # All functions below here help saving the list of optimizer states, one from each rank
 # build_unflat_state_dict is the interface used by FSDP
-def _extract_non_tensor_state(combined_state: Dict[int, Dict[str, List]], param_id: int) -> Dict:
-    non_tensor_state = {}  # This state is like the `step` count in Adam, not a tensor so we dont unpad or cat it.
+def _extract_non_tensor_state(
+    combined_state: Dict[int, Dict[str, List]], param_id: int
+) -> Dict:
+    non_tensor_state = (
+        {}
+    )  # This state is like the `step` count in Adam, not a tensor so we dont unpad or cat it.
     for k, v in combined_state[param_id].items():
         if torch.is_tensor(v[0]):
             continue
         elif len(set(v)) == 1:
             non_tensor_state[k] = v[0]
         else:
-            raise TypeError(f"Dont know how to consolidate optimizer param {k} with values {v}")
+            raise TypeError(
+                f"Dont know how to consolidate optimizer param {k} with values {v}"
+            )
     return non_tensor_state
 
 
@@ -121,7 +128,9 @@ def _unflatten_optim_state(
 
     # non_tensor_state refers to entries in sd[state][param_id] that are not tensors, like "step".
     # we check that these are identical across workers and then take the first
-    non_tensor_state = {id: _extract_non_tensor_state(combined_state, id) for id in combined_state}
+    non_tensor_state = {
+        id: _extract_non_tensor_state(combined_state, id) for id in combined_state
+    }
 
     # Local corresponds to flattened, global corresponds to unflattened.
     # Casting needed only for mypy.
@@ -147,13 +156,17 @@ def _unflatten_optim_state(
             #    3         None     FC2's bias, no flat state
             #    4         2        FC3's weight, second flat buffer (but with id 2)
             #    5         2        FC3's bias, second flat buffer (but with id 2)
-            global_to_local_id[next_global_id] = local_id if local_id in local_ids else None
+            global_to_local_id[next_global_id] = (
+                local_id if local_id in local_ids else None
+            )
             next_global_id += 1
     if not combined_state:
         return {}, global_to_local_id
 
     # copy non tensor state (like the "step" count) to all global entries
-    unflat_state = {i: copy.deepcopy(non_tensor_state[0]) for i in range(sum(num_global_params))}
+    unflat_state = {
+        i: copy.deepcopy(non_tensor_state[0]) for i in range(sum(num_global_params))
+    }
 
     # remove the global entries that don't have optim state because pytorch
     # optimizer's state_dict() function returns a state_dict without the missing
@@ -183,15 +196,22 @@ def _unflatten_optim_state(
             flat_buffer = torch.cat(v_unpad)
             if instance_list[local_id].flatten_parameters:
                 # Unflatten. Casting needed only for mypy.
-                param_views: Iterator = cast(FlattenParamsWrapper, instance_list[local_id]).get_param_views(
-                    [flat_buffer]
-                )
-                for global_id, param_view in zip(sorted(local_to_global[local_id]), param_views):
-                    assert k not in unflat_state[global_id], f"already added {k} to {global_id} {local_id}"
+                from oslo.torch.nn.parallel import FlattenParamsWrapper
+                param_views: Iterator = cast(
+                    FlattenParamsWrapper, instance_list[local_id]
+                ).get_param_views([flat_buffer])
+                for global_id, param_view in zip(
+                    sorted(local_to_global[local_id]), param_views
+                ):
+                    assert (
+                        k not in unflat_state[global_id]
+                    ), f"already added {k} to {global_id} {local_id}"
                     unflat_state[global_id][k] = param_view
             else:
                 # Copy non-flatten state directly.
-                assert len(local_to_global[local_id]) == 1, "Only support a single non-flatten parameter"
+                assert (
+                    len(local_to_global[local_id]) == 1
+                ), "Only support a single non-flatten parameter"
                 global_id = local_to_global[local_id][0]
                 unflat_state[global_id][k] = flat_buffer
             unflat_state[global_id].update(singleton_state[local_id])
@@ -227,17 +247,27 @@ def build_unflat_state_dict(
     # Use uncollected_opt_state to update tensor_state, singleton_state
     for local_id, v in uncollected_opt_state.items():
         assert local_id not in state
-        state[local_id] = {buffer_name: [x] for buffer_name, x in v.items() if not is_singleton_tensor(x)}
-        singleton_state[local_id] = {buffer_name: [x] for buffer_name, x in v.items() if is_singleton_tensor(x)}
+        state[local_id] = {
+            buffer_name: [x]
+            for buffer_name, x in v.items()
+            if not is_singleton_tensor(x)
+        }
+        singleton_state[local_id] = {
+            buffer_name: [x] for buffer_name, x in v.items() if is_singleton_tensor(x)
+        }
     # local ids are in the current state, global_ids will be in returned state.
-    unflat_state, global_to_local_id = _unflatten_optim_state(state, instance_list, world_pad_info, singleton_state)
+    unflat_state, global_to_local_id = _unflatten_optim_state(
+        state, instance_list, world_pad_info, singleton_state
+    )
     # Since there are no tensors in param_groups, deepcopy is fine.
     param_groups = copy.deepcopy(param_groups)
     # Casting needed only for mypy.
     num_params = sum([cast(int, m.num_params_managed) for m in instance_list])
     param_groups[0]["params"] = list(range(num_params))
     unflat_optim_state_dict = {
-        "state": dict(sorted(unflat_state.items())),  # NOTE: this is probably already sorted
+        "state": dict(
+            sorted(unflat_state.items())
+        ),  # NOTE: this is probably already sorted
         "param_id_map": global_to_local_id,
         "param_groups": param_groups,
         "uncollected_local_ids": list(uncollected_opt_state.keys()),
---
+++ tests/nn/multi_dimensional/test_zero1_tp1d.py
@@ -0,0 +1,75 @@
+import torch.distributed as dist
+import wandb
+from datasets import load_dataset
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer, GPT2LMHeadModel, GPT2Config
+
+from oslo.torch.distributed import ParallelContext
+from oslo.torch.nn.parallel.distributed.tensor_parallel import TensorParallel
+from oslo.torch.nn.parallel.utils import allocate_params
+
+# parallel context 생성
+from oslo.torch.optim import ZeroRedundancyOptimizer
+
+parallel_context = ParallelContext.from_torch(
+    data_parallel_size=2,
+    pipeline_parallel_size=1,
+    tensor_parallel_size=2,
+)
+
+# 토크나이저 생성
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+
+# 모델 생성 및 병렬화 수행
+model_no_tp = GPT2LMHeadModel(GPT2Config.from_pretrained("gpt2")).cuda()
+model_tp = GPT2LMHeadModel(GPT2Config.from_pretrained("gpt2"))
+wrapper_tp = TensorParallel(model_tp, parallel_context)
+allocate_params(wrapper_tp, parallel_context)
+# allocate_params 함수는 추후에 모든 페러렐 래퍼를 관장하는 클래스에서 처리될 예정
+# https://github.com/tunib-ai/oslo/blob/307131bbd5ed995ea8dca8ac541bfbce9bfec29b/oslo/pytorch/model_parallelism/model_parallel_engine.py
+
+if dist.get_rank() == 0:
+    print(wrapper_tp)
+
+# 옵티마이저 생성
+optimizer_zero_tp = ZeroRedundancyOptimizer(
+    Adam, wrapper_tp.parameters(), lr=3e-5, parallel_context=parallel_context
+)
+optimizer_no_tp = Adam(model_no_tp.parameters(), lr=3e-5)
+
+# 데이터셋 생성
+datasets = load_dataset("squad").data["train"]["context"]
+datasets = [str(sample) for sample in datasets[:500]]
+dataloader = DataLoader(datasets, batch_size=4)
+
+# 모니터링 생성
+if dist.get_rank() == 0:
+    wandb.init(project="oslo", name="zero+tp_exp")
+
+# 학습 시작
+for data in dataloader:
+    optimizer_zero_tp.zero_grad()
+    optimizer_no_tp.zero_grad()
+
+    inputs = tokenizer(
+        data,
+        return_tensors="pt",
+        padding=True,
+        truncation=True,
+        max_length=512,
+    ).to("cuda")
+
+    loss_tp = wrapper_tp(**inputs, labels=inputs["input_ids"]).loss
+    loss_no_tp = model_no_tp(**inputs, labels=inputs["input_ids"]).loss
+
+    if dist.get_rank() == 0:
+        print(f"ZERO+TP:{loss_tp}, NOTHING:{loss_no_tp}")
+        wandb.log({"zero+tp": loss_tp, "nothing": loss_no_tp})
+
+    loss_tp.backward()
+    loss_no_tp.backward()
+
+    optimizer_zero_tp.step()
+    optimizer_no_tp.step()
