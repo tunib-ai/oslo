d20178558eb653ed11f4ab8e4112cf75ab4728e8
granzou <loopinf@gmail.com>
2022-05-01 14:34:21 +0000

    test stopped in the middle

--- oslo/torch/nn/__init__.py
+++ oslo/torch/nn/__init__.py
@@ -10,18 +10,18 @@ from oslo.torch.nn.modules.functional import (
     mixed_dtype_fused_rms_norm_affine,
     fused_rms_norm,
 )
-from oslo.torch.nn.modules.conv import Conv1D, LazyConv1D
-from oslo.torch.nn.modules.dropout import (
-    FusedBiasDropout,
-    FusedBiasDropoutResidual,
-)
-from oslo.torch.nn.modules.linear import (
-    Linear,
-    LazyLinear,
-    ColumnParallelLinear,
-    RowParallelLinear,
-)
-from oslo.torch.nn.modules.embedding import LazyEmbedding, VocabParallelEmbedding1D
+#from oslo.torch.nn.modules.conv import Conv1D, LazyConv1D
+#from oslo.torch.nn.modules.dropout import (
+#    FusedBiasDropout,
+#    FusedBiasDropoutResidual,
+#)
+#from oslo.torch.nn.modules.linear import (
+#    Linear,
+#    LazyLinear,
+#    ColumnParallelLinear,
+#    RowParallelLinear,
+#)
+#from oslo.torch.nn.modules.embedding import LazyEmbedding, VocabParallelEmbedding1D
 
 from oslo.torch.nn.modules.fused_layer_norm import (
     FusedLayerNorm,
