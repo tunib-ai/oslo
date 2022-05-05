cfca732ee2940fb080b9d4535341c6ab608725af
ohwi <supebulous@gmail.com>
2022-05-05 13:47:12 +0900

    make forward test work
    forward is okay. need to test backward

--- tests/torch/nn/parallel/data_parallel/test_sp_multiheadattention.py
+++ tests/torch/nn/parallel/data_parallel/test_sp_multiheadattention.py
@@ -3,10 +3,8 @@ from torch.nn.modules.activation import MultiheadAttention as torchMHA
 
 from oslo.torch.distributed import ParallelContext
 from oslo.torch.nn.modules.activation import MultiheadAttention as osloMHA
-from oslo.torch.nn.parallel.data_parallel.sequence_parallel import (
-    SequenceParallel,
-)
-from oslo.torch.nn.parallel.utils import allocate_params
+from oslo.torch.nn.parallel.data_parallel.sequence_data_parallel import SequenceDataParallel
+from oslo.torch.distributed import ParallelContext, ParallelMode
 
 parallel_context = ParallelContext.from_torch(
     data_parallel_size=1,
@@ -18,29 +16,78 @@ parallel_context = ParallelContext.from_torch(
 
 d_model = 512
 nhead = 4
-dropout = 0.1
+dropout = 0.0
 batch_first = False
 
-torch_mha = torchMHA(d_model, nhead, dropout=dropout, batch_first=batch_first).cuda()
+# to make same dummy tensor between processes
+torch.manual_seed(0)
+
+# create dummy tensor whose shape is (L, B, E)
+sequence_length = 10
+dummy_tensor = torch.rand(sequence_length, 32, 512).double().cuda()
+
+# torch native
+# torch_mha = torchMHA(
+#     d_model,
+#     nhead,
+#     dropout=dropout,
+#     batch_first=batch_first,
+#
+# ).double()
+
+# tested and this makes same result with torch native
+torch_mha = osloMHA(
+    d_model,
+    nhead,
+    dropout=dropout,
+    batch_first=batch_first,
+    use_sequence_parallel=False,
+    parallel_context=parallel_context,
+).double()
+
+
+# oslo
 oslo_mha = osloMHA(
     d_model,
     nhead,
     dropout=dropout,
     batch_first=batch_first,
     use_sequence_parallel=True,
+    # use_sequence_parallel=False,
     parallel_context=parallel_context,
+).double()
+
+# copy weight data
+# oslo_mha.in_proj_weight.data.copy_(torch_mha.in_proj_weight.data.copy_)
+oslo_mha.load_state_dict(torch_mha.state_dict())
+
+# to cuda
+torch_mha.cuda()
+oslo_mha.cuda()
+
+# wrap sequence data parallel
+oslo_mha_wrapped = SequenceDataParallel(oslo_mha, parallel_context)
+
+torch_mha_output, torch_mha_output_weights = torch_mha(
+    dummy_tensor.detach(), dummy_tensor.detach(), dummy_tensor.detach(),
+    average_attn_weights=False,
 )
 
-allocate_params(oslo_mha, parallel_context)
-wrapper = SequenceParallel(oslo_mha, parallel_context)
+# retrieve sub tensor
+sub_seq_length = sequence_length // parallel_context.get_world_size(ParallelMode.SEQUENCE)
+start_idx = parallel_context.get_local_rank(ParallelMode.SEQUENCE) * sub_seq_length
+end_idx = start_idx + sub_seq_length
+dummy_tensor = dummy_tensor[start_idx:end_idx]
 
-dummy_tensor = torch.rand(10, 32, 512)
-torch_mha_output, torch_mha_output_wegiths = torch_mha(
-    dummy_tensor, dummy_tensor, dummy_tensor
-)
-oslo_mha_output, oslo_mha_output_wegiths = oslo_mha(
-    dummy_tensor, dummy_tensor, dummy_tensor
+oslo_mha_output, oslo_mha_output_weights = oslo_mha_wrapped(
+    dummy_tensor.detach(), dummy_tensor.detach(), dummy_tensor.detach()
 )
 
-print(torch.allclose(torch_mha_output, oslo_mha_output))
-print(torch.allclose(torch_mha_output_wegiths, oslo_mha_output_wegiths))
+
+def print_rank(s):
+    print(f"rank{parallel_context.get_global_rank()} {s}")
+
+
+# forward pass test
+print_rank(f"mha_output: {torch.allclose(torch_mha_output[start_idx:end_idx], oslo_mha_output)}")
+print_rank(f"mha_weights: {torch.allclose(torch_mha_output_weights[:, :, start_idx:end_idx], oslo_mha_output_weights)}")
