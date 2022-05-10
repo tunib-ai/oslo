e82fad59adad1748c50fed60c4624515a349e35d
hyunwoongko <gusdnd852@naver.com>
2022-05-10 15:03:02 +0900

    style code

--- tests/torch/nn/modules/test_fusedsoftmax.py
+++ tests/torch/nn/modules/test_fusedsoftmax.py
@@ -1,10 +1,15 @@
 from oslo.torch.nn import FusedScaleMaskSoftmax
 import torch
 
-softmax = FusedScaleMaskSoftmax(scale=1.1, use_triang_mask=True).cuda()
-inputs = torch.randn(10, 10, 10, 10).half().cuda()
+fused_softmax = FusedScaleMaskSoftmax(
+    scale=1.0, use_triang_mask=False, softmax_in_fp32=False
+).cuda()
+non_fused_softmax = FusedScaleMaskSoftmax(
+    scale=1.0, use_triang_mask=False, softmax_in_fp32=True
+).cuda()
 
-out = softmax(inputs)
-print(out)
+inputs = torch.randn(16, 16, 16, 16).half().cuda()
 
-
+fused_output = fused_softmax(inputs)
+non_fused_output = non_fused_softmax(inputs).half()
+print(f"All close: {torch.allclose(fused_output, non_fused_output, rtol=1e-2)}")
