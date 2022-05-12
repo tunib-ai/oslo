96843a108b9cd736ade725db99c60d62a1810985
granzou <loopinf@gmail.com>
2022-05-12 12:29:50 +0900

    style: Pass flake8
    Redefinition of unused name from line n (F811)
    
     import duplicated

--- oslo/torch/nn/__init__.py
+++ oslo/torch/nn/__init__.py
@@ -18,6 +18,12 @@ from oslo.torch.nn.modules.functional import (
     fused_gelu,
     multi_head_attention_forward,
     fused_scale_mask_softmax,
+    fused_rms_norm_affine,
+    fused_layer_norm,
+    mixed_dtype_fused_layer_norm_affine,
+    fused_layer_norm_affine,
+    mixed_dtype_fused_rms_norm_affine,
+    fused_rms_norm,
 )
 from oslo.torch.nn.modules.layer_norm import LayerNorm2D, LayerNorm2p5D
 from oslo.torch.nn.modules.linear import (
@@ -27,18 +33,6 @@ from oslo.torch.nn.modules.linear import (
     Linear2D,
     Linear2p5D,
     RowParallelLinear,
-)
-from oslo.torch.nn.modules.functional import (
-    fused_gelu,
-    fused_bias_gelu,
-    fused_bias_dropout,
-    fused_bias_dropout_residual,
-    fused_rms_norm_affine,
-    fused_layer_norm,
-    mixed_dtype_fused_layer_norm_affine,
-    fused_layer_norm_affine,
-    mixed_dtype_fused_rms_norm_affine,
-    fused_rms_norm,
 )
 
 from oslo.torch.nn.modules.fused_layer_norm import (
