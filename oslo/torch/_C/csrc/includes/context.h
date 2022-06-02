d3037d1c1a8886f8abf835c4016604ca994dca77
Taekyun Kim <brandon1609@naver.com>
2022-06-02 07:37:19 +0000

    chore: rm unnecessary c headers

--- oslo/torch/_C/csrc/includes/context.h
+++ oslo/torch/_C/csrc/includes/context.h
@@ -3,7 +3,6 @@
 #include "cublas_v2.h"
 #include "cuda.h"
 #include "curand.h"
-#include "gemm_test.h"
 #include <ATen/cuda/CUDAContext.h>
 #include <cassert>
 #include <cuda_runtime_api.h>
@@ -94,71 +93,10 @@ public:
 
   void SetSeed(uint64_t new_seed) { _seed = new_seed; }
 
-  void TestGemmFP16(bool test_gemm, int batch_size, int seq_len, int head_num,
-                    int size_per_head) {
-    // avoid rerun.
-    if (_gemm_algos.size() > 0)
-      return;
-
-    if (test_gemm) {
-      cublasHandle_t handle = GetCublasHandle();
-
-      std::unique_ptr<GemmTest<__half>> test_qkv_fw(
-          new GemmTest<__half>(batch_size * seq_len,     // M
-                               head_num * size_per_head, // N
-                               head_num * size_per_head, // K
-                               CUBLAS_OP_T, CUBLAS_OP_N, handle));
-
-      std::unique_ptr<GemmTest<__half>> test_inter(
-          new GemmTest<__half>(batch_size * seq_len,         // M
-                               4 * head_num * size_per_head, // N
-                               head_num * size_per_head,     // K
-                               CUBLAS_OP_T, CUBLAS_OP_N, handle));
-
-      std::unique_ptr<GemmTest<__half>> test_output(
-          new GemmTest<__half>(batch_size * seq_len,         // M
-                               head_num * size_per_head,     // N
-                               4 * head_num * size_per_head, // K
-                               CUBLAS_OP_T, CUBLAS_OP_N, handle));
-
-      std::unique_ptr<StridedGemmTest<__half>> test_attn_scores(
-          new StridedGemmTest<__half>(batch_size * head_num, // batch
-                                      seq_len,               // M
-                                      seq_len,               // N
-                                      size_per_head,         // K
-                                      CUBLAS_OP_T, CUBLAS_OP_N, handle));
-
-      std::unique_ptr<StridedGemmTest<__half>> test_attn_context(
-          new StridedGemmTest<__half>(batch_size * head_num, // batch
-                                      size_per_head,         // M
-                                      seq_len,               // N
-                                      seq_len,               // K
-                                      CUBLAS_OP_N, CUBLAS_OP_N, handle));
-
-      _gemm_algos.push_back(test_qkv_fw->TestAlgo(100));
-      _gemm_algos.push_back(test_inter->TestAlgo(100));
-      _gemm_algos.push_back(test_output->TestAlgo(100));
-      _gemm_algos.push_back(test_attn_scores->TestAlgo(100));
-      _gemm_algos.push_back(test_attn_context->TestAlgo(100));
-    } else {
-      // Use default algo.
-      _gemm_algos.push_back(std::array<int, 3>({99, 99, 99}));
-      _gemm_algos.push_back(std::array<int, 3>({99, 99, 99}));
-      _gemm_algos.push_back(std::array<int, 3>({99, 99, 99}));
-      _gemm_algos.push_back(std::array<int, 3>({99, 99, 99}));
-      _gemm_algos.push_back(std::array<int, 3>({99, 99, 99}));
-    }
-  }
-
-  const std::vector<std::array<int, 3>> &GetGemmAlgos() const {
-    return _gemm_algos;
-  }
-
 private:
   curandGenerator_t _gen;
   cublasHandle_t _cublasHandle;
   void *_workspace;
   uint64_t _seed;
   uint64_t _curr_offset;
-  std::vector<std::array<int, 3>> _gemm_algos;
 };
--- oslo/torch/_C/csrc/includes/custom_cuda_layers.h
+++ oslo/torch/_C/csrc/includes/custom_cuda_layers.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "cuda_fp16.h"
+
 void launch_param_update(const float *input, __half *output, int size,
                          cudaStream_t stream);
 void launch_param_update_half(const float *input, __half *output, int size,
