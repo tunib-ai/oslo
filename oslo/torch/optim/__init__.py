ab95391bac466c42c4bf32e5399a13ae474c7c1b
tree-park <hspark.engineer@gmail.com>
2022-07-17 01:54:54 +0900

    remove import bugs at other sources not trainer

--- oslo/torch/optim/__init__.py
+++ oslo/torch/optim/__init__.py
@@ -2,5 +2,5 @@ from oslo.torch.optim.zero_redundancy_optimizer import ZeroRedundancyOptimizer
 from oslo.torch.optim.fused_adam import FusedAdam
 from oslo.torch.optim.fused_adagrad import FusedAdagrad
 from oslo.torch.optim.fused_sgd import FusedSGD
-from oslo.torch.optim.fused_novograd import FusedNovograd
-from oslo.torch.optim.fused_lamb import FuseLamb
+from oslo.torch.optim.fused_novograd import FusedNovoGrad
+from oslo.torch.optim.fused_lamb import FusedLamb
