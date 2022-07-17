d8f33f024b0ff546dec2ea1f5087dd8a26130bf4
Taekyun Kim <taekkim@student.42seoul.kr>
2022-07-17 06:53:11 +0000

    add: FusedNovograd, FusedLamb, FusedMixedPrecisionLamb

--- oslo/torch/optim/__init__.py
+++ oslo/torch/optim/__init__.py
@@ -4,3 +4,4 @@ from oslo.torch.optim.fused_adagrad import FusedAdagrad
 from oslo.torch.optim.fused_sgd import FusedSGD
 from oslo.torch.optim.fused_novograd import FusedNovograd
 from oslo.torch.optim.fused_lamb import FusedLamb
+from oslo.torch.optim.fused_mixed_precision_lamb import FusedMixedPrecisionLamb
--- oslo/torch/optim/fused_lamb.py
+++ oslo/torch/optim/fused_lamb.py
@@ -1,39 +1,65 @@
-"""
-Copyright 2019 The Microsoft DeepSpeed Team
-
-Copyright NVIDIA/apex
-This file is adapted from NVIDIA/apex/optimizer/fused_adam and implements the LAMB optimizer
-"""
-import types
 import torch
-from oslo.torch._C import get_lamb_kernel
+from oslo.torch.utils.multi_tensor_apply import multi_tensor_applier
+from oslo.torch._C import get_lamb_kernel, get_l2norm_kernel
 
 
 class FusedLamb(torch.optim.Optimizer):
-    """Implements the LAMB algorithm. Currently GPU-only.
 
-    LAMB was proposed in `Large Batch Optimization for Deep Learning: Training BERT in 76 minutes.
-    https://arxiv.org/abs/1904.00962
+    """Implements LAMB algorithm.
+
+    Currently GPU-only.  Requires Apex to be installed via
+    ``pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./``.
+
+    This version of fused LAMB implements 2 fusions.
+
+      * Fusion of the LAMB update's elementwise operations
+      * A multi-tensor apply launch that batches the elementwise updates applied to all the model's parameters into one or a few kernel launches.
+
+    :class:`apex.optimizers.FusedLAMB`'s usage is identical to any ordinary Pytorch optimizer::
+
+        opt = apex.optimizers.FusedLAMB(model.parameters(), lr = ....)
+        ...
+        opt.step()
+
+    :class:`apex.optimizers.FusedLAMB` may be used with or without Amp.  If you wish to use :class:`FusedLAMB` with Amp,
+    you may choose any ``opt_level``::
+
+        opt = apex.optimizers.FusedLAMB(model.parameters(), lr = ....)
+        model, opt = amp.initialize(model, opt, opt_level="O0" or "O1 or "O2")
+        ...
+        opt.step()
+
+    In general, ``opt_level="O1"`` is recommended.
+
+    LAMB was proposed in `Large Batch Optimization for Deep Learning: Training BERT in 76 minutes`_.
 
     Arguments:
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups.
         lr (float, optional): learning rate. (default: 1e-3)
-        bias_correction (bool, optional): bias correction (default: True)
         betas (Tuple[float, float], optional): coefficients used for computing
-            running averages of gradient and its square. (default: (0.9, 0.999))
+            running averages of gradient and its norm. (default: (0.9, 0.999))
         eps (float, optional): term added to the denominator to improve
             numerical stability. (default: 1e-8)
-        eps_inside_sqrt (boolean, optional): in the 'update parameters' step,
-            adds eps to the bias-corrected second moment estimate before
-            evaluating square root instead of adding it to the square root of
-            second moment estimate as in the original paper. (default: False)
         weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        amsgrad (boolean, optional): whether to use the AMSGrad variant of this
+            algorithm from the paper `On the Convergence of Adam and Beyond`_
+            NOT SUPPORTED now! (default: False)
+        adam_w_mode (boolean, optional): Apply L2 regularization or weight decay
+            True for decoupled weight decay(also known as AdamW) (default: True)
+        grad_averaging (bool, optional): whether apply (1-beta2) to grad when
+            calculating running averages of gradient. (default: True)
+        set_grad_none (bool, optional): whether set grad to None when zero_grad()
+            method is called. (default: True)
         max_grad_norm (float, optional): value used to clip global grad norm
-            (default: 0.0)
-        max_coeff(float, optional): maximum value of the lamb coefficient (default: 10.0)
-        min_coeff(float, optional): minimum value of the lamb coefficient (default: 0.01)
-        amsgrad (boolean, optional): NOT SUPPORTED in FusedLamb!
+            (default: 1.0)
+        use_nvlamb (boolean, optional): Apply adaptive learning rate to 0.0
+            weight decay parameter (default: False)
+
+    .. _Large Batch Optimization for Deep Learning - Training BERT in 76 minutes:
+        https://arxiv.org/abs/1904.00962
+    .. _On the Convergence of Adam and Beyond:
+        https://openreview.net/forum?id=ryQu7f-RZ
     """
 
     def __init__(
@@ -42,16 +68,15 @@ class FusedLamb(torch.optim.Optimizer):
         lr=1e-3,
         bias_correction=True,
         betas=(0.9, 0.999),
-        eps=1e-8,
-        eps_inside_sqrt=False,
-        weight_decay=0.0,
-        max_grad_norm=0.0,
-        max_coeff=10.0,
-        min_coeff=0.01,
+        eps=1e-6,
+        weight_decay=0.01,
         amsgrad=False,
+        adam_w_mode=True,
+        grad_averaging=True,
+        set_grad_none=True,
+        max_grad_norm=1.0,
+        use_nvlamb=False,
     ):
-        self.fused_lamb_cuda = get_lamb_kernel()
-
         if amsgrad:
             raise RuntimeError("FusedLamb does not support the AMSGrad variant.")
         defaults = dict(
@@ -60,142 +85,156 @@ class FusedLamb(torch.optim.Optimizer):
             betas=betas,
             eps=eps,
             weight_decay=weight_decay,
+            grad_averaging=grad_averaging,
             max_grad_norm=max_grad_norm,
-            max_coeff=max_coeff,
-            min_coeff=min_coeff,
         )
         super(FusedLamb, self).__init__(params, defaults)
-        self.eps_mode = 0 if eps_inside_sqrt else 1
-        self.lamb_coeffs = []
+        # TODO
+        self.multi_tensor_l2norm = get_l2norm_kernel().multi_tensor_l2norm
+        self._dummy_overflow_buf = torch.tensor(
+            [0], dtype=torch.int, device=self.param_groups[0]["params"][0].device
+        )
+        self.multi_tensor_lamb = get_lamb_kernel().multi_tensor_lamb
 
-    def step(
-        self, closure=None, grads=None, output_params=None, scale=1.0, grad_norms=None
-    ):
+        self.adam_w_mode = 1 if adam_w_mode else 0
+        self.set_grad_none = set_grad_none
+        self.use_nvlamb = use_nvlamb
+
+    def zero_grad(self):
+        if self.set_grad_none:
+            for group in self.param_groups:
+                for p in group["params"]:
+                    p.grad = None
+        else:
+            super(FusedLamb, self).zero_grad()
+
+    def step(self, closure=None):
         """Performs a single optimization step.
 
         Arguments:
             closure (callable, optional): A closure that reevaluates the model
                 and returns the loss.
-            grads (list of tensors, optional): weight gradient to use for the
-                optimizer update. If gradients have type torch.half, parameters
-                are expected to be in type torch.float. (default: None)
-            output params (list of tensors, optional): A reduced precision copy
-                of the updated weights written out in addition to the regular
-                updated weights. Have to be of same type as gradients. (default: None)
-            scale (float, optional): factor to divide gradient tensor values
-                by before applying to weights. (default: 1)
         """
         loss = None
         if closure is not None:
             loss = closure()
 
-        if grads is None:
-            grads_group = [None] * len(self.param_groups)
-        # backward compatibility
-        # assuming a list/generator of parameter means single group
-        elif isinstance(grads, types.GeneratorType):
-            grads_group = [grads]
-        elif type(grads[0]) != list:
-            grads_group = [grads]
-        else:
-            grads_group = grads
-
-        if output_params is None:
-            output_params_group = [None] * len(self.param_groups)
-        elif isinstance(output_params, types.GeneratorType):
-            output_params_group = [output_params]
-        elif type(output_params[0]) != list:
-            output_params_group = [output_params]
-        else:
-            output_params_group = output_params
-
-        if grad_norms is None:
-            grad_norms = [None] * len(self.param_groups)
-
-        # remove the previous coeffs
-        del self.lamb_coeffs[:]
-
-        for group, grads_this_group, output_params_this_group, grad_norm_group in zip(
-            self.param_groups, grads_group, output_params_group, grad_norms
-        ):
-            if grads_this_group is None:
-                grads_this_group = [None] * len(group["params"])
-            if output_params_this_group is None:
-                output_params_this_group = [None] * len(group["params"])
-
-            if grad_norm_group is None:
-                grad_norm_group = [None] * len(group["params"])
-            elif not isinstance(grad_norm_group, list):
-                grad_norm_group = [grad_norm_group]
-
-            bias_correction = 1 if group["bias_correction"] else 0
-
-            for p, grad, output_param, grad_norm in zip(
-                group["params"],
-                grads_this_group,
-                output_params_this_group,
-                grad_norm_group,
-            ):
-
-                # compute combined scale factor for this group
-                combined_scale = scale
-                if group["max_grad_norm"] > 0:
-                    # norm is in fact norm*scale
-                    clip = ((grad_norm / scale) + 1e-6) / group["max_grad_norm"]
-                    if clip > 1:
-                        combined_scale = clip * scale
-
-                # note: p.grad should not ever be set for correct operation of mixed precision optimizer that sometimes sends None gradients
-                if p.grad is None and grad is None:
+        # create separate grad lists for fp32 and fp16 params
+        g_all_32, g_all_16 = [], []
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
                     continue
-                if grad is None:
-                    grad = p.grad.data
-                if grad.is_sparse:
-                    raise RuntimeError("FusedLamb does not support sparse gradients")
+                if p.dtype == torch.float32:
+                    g_all_32.append(p.grad.data)
+                elif p.dtype == torch.float16:
+                    g_all_16.append(p.grad.data)
+                else:
+                    raise RuntimeError("FusedLamb only support fp16 and fp32.")
+
+        device = self.param_groups[0]["params"][0].device
+        g_norm_32, g_norm_16 = torch.zeros(1, device=device), torch.zeros(
+            1, device=device
+        )
+        # compute grad norm for two lists
+        if len(g_all_32) > 0:
+            g_norm_32 = multi_tensor_applier(
+                self.multi_tensor_l2norm, self._dummy_overflow_buf, [g_all_32], False
+            )[0]
+        if len(g_all_16) > 0:
+            g_norm_16 = multi_tensor_applier(
+                self.multi_tensor_l2norm, self._dummy_overflow_buf, [g_all_16], False
+            )[0]
+
+        # blend two grad norms to get global grad norm
+        global_grad_norm = multi_tensor_applier(
+            self.multi_tensor_l2norm,
+            self._dummy_overflow_buf,
+            [[g_norm_32, g_norm_16]],
+            False,
+        )[0]
+        max_grad_norm = self.defaults["max_grad_norm"]
+
+        for group in self.param_groups:
+            bias_correction = 1 if group["bias_correction"] else 0
+            beta1, beta2 = group["betas"]
+            grad_averaging = 1 if group["grad_averaging"] else 0
+
+            # assume same step across group now to simplify things
+            # per parameter step can be easily support by making it tensor, or pass list into kernel
+            if "step" in group:
+                group["step"] += 1
+            else:
+                group["step"] = 1
+
+            # create lists for multi-tensor apply
+            g_16, p_16, m_16, v_16 = [], [], [], []
+            g_32, p_32, m_32, v_32 = [], [], [], []
+
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                if p.grad.data.is_sparse:
+                    raise RuntimeError(
+                        "FusedLamb does not support sparse gradients, please consider SparseAdam instead"
+                    )
 
                 state = self.state[p]
-
                 # State initialization
                 if len(state) == 0:
-                    state["step"] = 0
                     # Exponential moving average of gradient values
                     state["exp_avg"] = torch.zeros_like(p.data)
-                    # Exponential moving average of squared gradient values
+                    # Exponential moving average of gradient values
                     state["exp_avg_sq"] = torch.zeros_like(p.data)
 
-                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
-                beta1, beta2 = group["betas"]
-                max_coeff = group["max_coeff"]
-                min_coeff = group["min_coeff"]
+                if p.dtype == torch.float16:
+                    g_16.append(p.grad.data)
+                    p_16.append(p.data)
+                    m_16.append(state["exp_avg"])
+                    v_16.append(state["exp_avg_sq"])
+                elif p.dtype == torch.float32:
+                    g_32.append(p.grad.data)
+                    p_32.append(p.data)
+                    m_32.append(state["exp_avg"])
+                    v_32.append(state["exp_avg_sq"])
+                else:
+                    raise RuntimeError("FusedLamb only support fp16 and fp32.")
 
-                state["step"] += 1
-
-                out_p = (
-                    torch.tensor([], dtype=torch.float)
-                    if output_param is None
-                    else output_param
-                )
-                lamb_coeff = self.fused_lamb_cuda.lamb(
-                    p.data,
-                    out_p,
-                    exp_avg,
-                    exp_avg_sq,
-                    grad,
+            if len(g_16) > 0:
+                multi_tensor_applier(
+                    self.multi_tensor_lamb,
+                    self._dummy_overflow_buf,
+                    [g_16, p_16, m_16, v_16],
                     group["lr"],
                     beta1,
                     beta2,
-                    max_coeff,
-                    min_coeff,
                     group["eps"],
-                    combined_scale,
-                    state["step"],
-                    self.eps_mode,
+                    group["step"],
                     bias_correction,
                     group["weight_decay"],
+                    grad_averaging,
+                    self.adam_w_mode,
+                    global_grad_norm,
+                    max_grad_norm,
+                    self.use_nvlamb,
                 )
-                self.lamb_coeffs.append(lamb_coeff)
-        return loss
+            if len(g_32) > 0:
+                multi_tensor_applier(
+                    self.multi_tensor_lamb,
+                    self._dummy_overflow_buf,
+                    [g_32, p_32, m_32, v_32],
+                    group["lr"],
+                    beta1,
+                    beta2,
+                    group["eps"],
+                    group["step"],
+                    bias_correction,
+                    group["weight_decay"],
+                    grad_averaging,
+                    self.adam_w_mode,
+                    global_grad_norm,
+                    max_grad_norm,
+                    self.use_nvlamb,
+                )
 
-    def get_lamb_coeffs(self):
-        lamb_coeffs = [lamb_coeff.item() for lamb_coeff in self.lamb_coeffs]
-        return lamb_coeffs
+        return loss
