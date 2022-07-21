3a177b3b5d9981e001bcec17191b5293ea4d1b44
gimmaru <john8778@naver.com>
2022-07-22 01:43:15 +0900

    Update oslo config

--- oslo/transformers/oslo_init.py
+++ oslo/transformers/oslo_init.py
@@ -1,39 +1,83 @@
-import importlib.util
 import json
 import logging
 from copy import deepcopy
 import torch
-from typing import List
+from typing import List, Tuple
 
 from oslo.torch.distributed import ParallelContext, ParallelMode
-
-from oslo.torch.nn.parallel.data_parallel.sequence_data_parallel import (
-    SequenceDataParallel,)
-
-
 from oslo.torch.nn.parallel import (
     PipelineParallel,
     TensorParallel,
     ShardedDataParallel,
     FullyShardedDataParallel,
     DistributedDataParallel,
+    SequenceDataParallel,
 )
 
 
 def _type(_type):
     return lambda key, val: {
-        "check":
-            isinstance(val, _type),
-        "msg":
-            f"``{key}: {val}`` is not a valid set. it must be type of {_type}",
+        "check": isinstance(val, _type),
+        "msg": f"``{key}: {val}`` is not a valid set. it must be type of {_type}",
     }
 
 
 SUPPORTED_FEATURES = {
     "data_parallelism": {
-        "stage": _type(str),
         "data_parallel_size": _type(int),
         "sequence_parallel_size": _type(int),
+        "zero2": {
+            "broadcast_buffers": _type(bool),
+            "sync_models_at_startup": _type(bool),
+            "reduce_buffer_size": _type(int),
+            "auto_refresh_trainable": _type(bool),
+            "reduce_fp16": _type(bool),
+            "warn_on_trainable_params_changed": _type(bool),
+        },
+        "zero3": {
+            "reshard_after_forward": _type((bool, type(None))),
+            "disable_reshard_on_root": _type((bool, type(None))),
+            "mixed_precision": _type((bool, type(None))),
+            "fp32_reduce_scatter": _type((bool, type(None))),
+            "flatten_parameters": _type((bool, type(None))),
+            "move_params_to_cpu": _type((bool, type(None))),
+            "compute_dtype": _type((torch.dtype, type(None))),
+            "buffer_dtype": _type((torch.dtype, type(None))),
+            "move_grads_to_cpu": _type((bool, type(None))),
+            "bucket_cap_mb": _type((int, type(None))),
+            "compute_device": _type((torch.device, type(None))),
+            "no_broadcast_optim_state": _type((bool, type(None))),
+            "state_dict_device": _type((torch.device, type(None))),
+            "clear_autocast_cache": _type(bool),
+            "force_input_to_fp32": _type(bool),
+            "verbose": _type(bool),
+            "cpu_offload": _type((bool, type(None))),
+            "offload_config": {
+                "offload_type": _type((str, type(None))),
+                "dir": _type((str, type(None))),
+            },
+            "state_dict_on_rank_0_only": _type(bool),
+        },
+        "distributed": {
+            "device_ids": _type((list, torch.device, type(None))),
+            "output_device": _type((int, torch.device, type(None))),
+            "dim": _type(int),
+            "broadcast_buffers": _type(bool),
+            "bucket_cap_mb": _type(int),
+            "find_unused_parameters": _type(bool),
+            "check_reduction": _type(bool),
+            "gradient_as_bucket_view": type(bool),
+        },
+        "sequence": {
+            "device_ids": _type((list, torch.device, type(None))),
+            "output_device": _type((int, torch.device, type(None))),
+            "dim": _type(int),
+            "broadcast_buffers": _type(bool),
+            "bucket_cap_mb": _type(int),
+            "find_unused_parameters": _type(bool),
+            "check_reduction": _type(bool),
+            "gradient_as_bucket_view": type(bool),
+        },
     },
     "model_parallelism": {
         "expert_parallel_size": _type(int),
@@ -41,20 +85,21 @@ SUPPORTED_FEATURES = {
         "tensor_parallel_size": _type(int),
         "tensor_parallel_depth": _type(int),
         "tensor_parallel_mode": _type(str),
+        "pipline": {
+            "memory_computation_balance": _type(float),
+            "tracing_inputs": _type(dict, type(None)),
+        },
+        "tensor": {"mapping": _type(dict, type(None))},
     },
     "activation_checkpointing": {
         "partitioned_checkpointing": _type(bool),
         "contiguous_checkpointing": _type(bool),
         "cpu_checkpointing": _type(bool),
     },
-    "kernel_fusion": {
-        "memory_efficient_fusion": _type(bool),
-        "custom_cuda_kernels": _type(list),
-    },
     "lazy_initialization": _type(bool),
     "backend": _type(str),
-    "seed": _type(int),
 }
+
 
 TENSOR_PARALLEL_MODE_TYPES = {
     "1d": ParallelMode.TENSOR_1D,
@@ -65,24 +110,28 @@ TENSOR_PARALLEL_MODE_TYPES = {
 
 
 def _config_check(arg, user_config):
-    assert len(user_config) > 0, f"There are no arguments in dictionary."
+    assert len(user_config) > 0, "There are no arguments in dictionary."
 
     if isinstance(user_config, dict):
         for k in user_config:
             if isinstance(arg, dict):
                 assert k in arg, (
                     f"An argument ``{k}`` is not available. "
-                    f"We only support the arguments like {list(arg.keys())}.")
+                    f"We only support the arguments like {list(arg.keys())}."
+                )
             else:
-                raise Exception(f"``{k}: {user_config[k]} is not a valid set. "
-                                f"please check your configuration.``")
+                raise Exception(
+                    f"``{k}: {user_config[k]} is not a valid set. "
+                    f"please check your configuration.``"
+                )
 
             if isinstance(user_config[k], dict):
                 _config_check(arg[k], user_config[k])
             else:
                 assert not isinstance(arg[k], dict), (
                     f"``{k}: {user_config[k]} is not a valid set. "
-                    f"please check your configuration.``")
+                    f"please check your configuration.``"
+                )
 
                 check_result = arg[k](k, user_config[k])
                 assert check_result["check"], check_result["msg"]
@@ -92,10 +141,13 @@ def _config_check(arg, user_config):
 
 def check_user_config(user_config):
     _config_check(SUPPORTED_FEATURES, user_config)
-    assert user_config['model_parallelism'][
-        'tensor_parallel_mode'] in TENSOR_PARALLEL_MODE_TYPES.keys(), (
-            f"{user_config['model_parallelism']['tensor_parallel_mode']} is not valid type of tensor_parallel_mode. "
-            f"choose one of {', '.join(TENSOR_PARALLEL_MODE_TYPES.keys())}")
+    assert (
+        user_config["model_parallelism"]["tensor_parallel_mode"]
+        in TENSOR_PARALLEL_MODE_TYPES.keys()
+    ), (
+        f"{user_config['model_parallelism']['tensor_parallel_mode']} is not valid type of tensor_parallel_mode. "
+        f"choose one of {', '.join(TENSOR_PARALLEL_MODE_TYPES.keys())}"
+    )
 
 
 class OsloTrainerConfig:
@@ -106,9 +158,60 @@ class OsloTrainerConfig:
     json file or dictionary form should be like the following:
         {
             "data_parallelism": {
-                "stage": _type(str),
                 "data_parallel_size": _type(int),
                 "sequence_parallel_size": _type(int),
+                "zero2": {
+                    "broadcast_buffers": _type(bool),
+                    "sync_models_at_startup": _type(bool),
+                    "reduce_buffer_size": _type(int),
+                    "auto_refresh_trainable": _type(bool),
+                    "reduce_fp16": _type(bool),
+                    "warn_on_trainable_params_changed": _type(bool),
+                },
+                "zero3": {
+                    "reshard_after_forward": _type((bool, type(None))),
+                    "disable_reshard_on_root": _type((bool, type(None))),
+                    "mixed_precision": _type((bool, type(None))),
+                    "fp32_reduce_scatter": _type((bool, type(None))),
+                    "flatten_parameters": _type((bool, type(None))),
+                    "move_params_to_cpu": _type((bool, type(None))),
+                    "compute_dtype": _type((torch.dtype, type(None))),
+                    "buffer_dtype": _type((torch.dtype, type(None))),
+                    "move_grads_to_cpu": _type((bool, type(None))),
+                    "bucket_cap_mb": _type((int, type(None))),
+                    "compute_device": _type((torch.device, type(None))),
+                    "no_broadcast_optim_state": _type((bool, type(None))),
+                    "state_dict_device": _type((torch.device, type(None))),
+                    "clear_autocast_cache": _type(bool),
+                    "force_input_to_fp32": _type(bool),
+                    "verbose": _type(bool),
+                    "cpu_offload": _type((bool, type(None))),
+                    "offload_config": {
+                        "offload_type": _type((str, type(None))),
+                        "dir": _type((str, type(None))),
+                    },
+                    "state_dict_on_rank_0_only": _type(bool),
+                },
+                "distributed": {
+                    "device_ids": _type((list, torch.device, type(None))),
+                    "output_device": _type((int, torch.device, type(None))),
+                    "dim": _type(int),
+                    "broadcast_buffers": _type(bool),
+                    "bucket_cap_mb": _type(int),
+                    "find_unused_parameters": _type(bool),
+                    "check_reduction": _type(bool),
+                    "gradient_as_bucket_view": type(bool),
+                },
+                "sequence": {
+                    "device_ids": _type((list, torch.device, type(None))),
+                    "output_device": _type((int, torch.device, type(None))),
+                    "dim": _type(int),
+                    "broadcast_buffers": _type(bool),
+                    "bucket_cap_mb": _type(int),
+                    "find_unused_parameters": _type(bool),
+                    "check_reduction": _type(bool),
+                    "gradient_as_bucket_view": type(bool),
+                },
             },
             "model_parallelism": {
                 "expert_parallel_size": _type(int),
@@ -116,30 +219,29 @@ class OsloTrainerConfig:
                 "tensor_parallel_size": _type(int),
                 "tensor_parallel_depth": _type(int),
                 "tensor_parallel_mode": _type(str),
+                "pipline": {
+                    "memory_computation_balance": _type(float),
+                    "tracing_inputs": _type(dict, type(None)),
+                },
+                "tensor": {"mapping": _type(dict, type(None))},
             },
             "activation_checkpointing": {
                 "partitioned_checkpointing": _type(bool),
                 "contiguous_checkpointing": _type(bool),
                 "cpu_checkpointing": _type(bool),
             },
-            "kernel_fusion": {
-                "memory_efficient_fusion": _type(bool),
-                "custom_cuda_kernels": _type(list),
-            },
             "lazy_initialization": _type(bool),
             "backend": _type(str),
-            "seed": _type(int)
         }
 
     Args:
-        config_file_or_dict (`Union[str, Dict]`): path to DeepSpeed config file or dict.
+        config_file_or_dict (`Union[str, Dict]`): path to Oslo user config file or dict.
 
     """
 
     DP = "data_parallelism"
     MP = "model_parallelism"
     ACTIVE_CHECKPOINT = "activation_checkpointing"
-    KERNEL_FUSION = "kernel_fusion"
     LAZY_INIT = "lazy_initialization"
     BACKEND = "backend"
 
@@ -155,15 +257,15 @@ class OsloTrainerConfig:
             with open(config_file_or_dict, "r", encoding="utf-8") as f:
                 config = json.load(f)
         else:
-            raise ValueError(
-                "expecting either a path to a oslo config file or a dict")
+            raise ValueError("expecting either a path to a oslo config file or a dict")
 
         check_user_config(config)
 
         # check user config tensor_parallel_mode
-        user_tp_mode = config['model_parallelism']['tensor_parallel_mode']
-        config['model_parallelism'][
-            'tensor_parallel_mode'] = TENSOR_PARALLEL_MODE_TYPES[user_tp_mode]
+        user_tp_mode = config["model_parallelism"]["tensor_parallel_mode"]
+        config["model_parallelism"][
+            "tensor_parallel_mode"
+        ] = TENSOR_PARALLEL_MODE_TYPES[user_tp_mode]
         # check user config data_parallel dp_stage
 
         self._set_config(config)
@@ -189,16 +291,21 @@ class OsloTrainerConfig:
 
     def dtype(self):
         if self._dtype is None:
-            raise ValueError(
-                "trainer_config_process() wasn't called yet to tell dtype")
+            raise ValueError("trainer_config_process() wasn't called yet to tell dtype")
         return self._dtype
 
-    def adjust_train_args(self, args):
-        self.train_batch_size = args.world_size * args.per_device_train_batch_size * args.gradient_accumulation_steps
-        # if args.train_batch_size != train_batch_size:
+    # def adjust_train_args(self, args):
+    # self.train_batch_size = (
+    #     args.world_size
+    #     * args.per_device_train_batch_size
+    #     * args.gradient_accumulation_steps
+    # )
+    # if args.train_batch_size != train_batch_size:
 
 
-def init_oslo_features(oslo_init_config: OsloTrainerConfig) -> (ParallelContext, List):
+def init_oslo_features(
+    oslo_init_config: OsloTrainerConfig,
+) -> Tuple[ParallelContext, List]:
     """
     Init OSLO features with json or dict configuration user passed.
     ParallelContext or other effective features should be defined on this function
@@ -215,15 +322,29 @@ def init_oslo_features(oslo_init_config: OsloTrainerConfig) -> (ParallelContext,
     cfg = oslo_init_config
 
     parallel_context = ParallelContext.from_torch(
-        data_parallel_size=cfg.data_parallel_size,
-        sequence_parallel_size=cfg.sequence_parallel_size,
-        expert_parallel_size=cfg.expert_parallel_size,
-        pipeline_parallel_size=cfg.pipeline_parallel_size,
-        tensor_parallel_size=cfg.tensor_parallel_size,
-        tensor_parallel_depth=cfg.tensor_parallel_depth,
-        tensor_parallel_mode=cfg.tensor_parallel_mode,
-        backend=cfg.backend,
-        seed=cfg.seed,
+        data_parallel_size=cfg.data_parallel_size
+        if hasattr(cfg, "data_parallel_size")
+        else 1,
+        sequence_parallel_size=cfg.sequence_parallel_size
+        if hasattr(cfg, "sequence_parallel_size")
+        else 1,
+        expert_parallel_size=cfg.expert_parallel_size
+        if hasattr(cfg, "expert_parallel_size")
+        else 1,
+        pipeline_parallel_size=cfg.pipeline_parallel_size
+        if hasattr(cfg, "pipeline_parallel_size")
+        else 1,
+        tensor_parallel_size=cfg.tensor_parallel_size
+        if hasattr(cfg, "tensor_parallel_size")
+        else 1,
+        tensor_parallel_depth=cfg.tensor_parallel_depth
+        if hasattr(cfg, "tensor_parallel_depth")
+        else None,
+        tensor_parallel_mode=cfg.tensor_parallel_mode
+        if hasattr(cfg, "tensor_parallel_mode")
+        else TENSOR_PARALLEL_MODE_TYPES["1d"],
+        backend=cfg.backend if hasattr(cfg, "backend") else "nccl",
+        seed=cfg.seed if hasattr(cfg, "seed") else 42,
     )
 
     if cfg.tensor_parallel_size > 1 and cfg.sequence_parallel_size > 1:
@@ -231,26 +352,31 @@ def init_oslo_features(oslo_init_config: OsloTrainerConfig) -> (ParallelContext,
             "TensorParallel and SequenceParallel can't not be used at the same time. Modify oslo config to avoid wrong parallel setting"
         )
     model_wrapper = []
-    if cfg.tensor_parallel_size > 1:
-        model_wrapper.append(TensorParallel)
     if cfg.sequence_parallel_size > 1:
         model_wrapper.append(SequenceDataParallel)
+    if cfg.data_parallel_size > 1:
+        if hasattr(cfg, "distributed"):
+            model_wrapper.append(DistributedDataParallel)
+        if hasattr(cfg, "zero3"):
+            model_wrapper.append(FullyShardedDataParallel)
+        elif hasattr(cfg, "zero2"):
+            model_wrapper.append(ShardedDataParallel)
+        if hasattr(cfg, "zero2") and hasattr(cfg, "zero3"):
+            logging.warning(
+                "Both ZeRO2 and ZeRO3 are set, Ignore ZeRO2 and apply ZeRO3."
+            )
+        if (
+            not hasattr(cfg, "distributed")
+            and not hasattr(cfg, "zero2")
+            and not hasattr(cfg, "zero3")
+        ):
+            logging.warning(
+                "No delivered stage for data_parallelism, default mode DistributedDataParallel is set"
+            )
+            model_wrapper.append(DistributedDataParallel)
+    if cfg.tensor_parallel_size > 1:
+        model_wrapper.append(TensorParallel)
     if cfg.pipeline_parallel_size > 1:
         model_wrapper.append(PipelineParallel)
-    if cfg.data_parallel_size > 1:
-        if hasattr(cfg, 'stage'):
-            if cfg.stage == 'zero2':
-                model_wrapper.append(ShardedDataParallel)
-            elif cfg.stage == 'zero3':
-                model_wrapper.append(FullyShardedDataParallel)
-            elif cfg.stage == 'ddp':
-                model_wrapper.append(DistributedDataParallel)
-            else:
-                raise AttributeError(
-                    "OSLO supports zero stage 1&2 and 3 that are respectively ShardedDataParallel and FullyShardedDataParallel."
-                )
-        else:
-            logging.warning("No delivered stage for data_parallelism, default mode DistributedDataParallel is set")
-            model_wrapper.append(DistributedDataParallel)
 
     return parallel_context, model_wrapper
