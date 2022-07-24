cf2422ab58cd16160471c45eba9646e64c584b49
gimmaru <john8778@naver.com>
2022-07-24 20:59:09 +0900

    add parallel key for sp

--- oslo/transformers/tasks/data_base.py
+++ oslo/transformers/tasks/data_base.py
@@ -9,6 +9,29 @@ except ImportError:
     print("You have to install `transformers` to use `oslo.transformers` modules")
 
 
+PARALLEL_KEY = {
+    "clm": ("input_ids", "attention_mask"),
+    "mlm": ("input_ids", "attention_mask"),
+    "seq_cls": ("input_ids", "attention_mask"),
+    "token_cls": ("input_ids", "attention_mask"),
+    "summarization": ("input_ids", "attention_mask"),
+    "bert": ("input_ids", "token_type_ids", "attention_mask"),
+    "albert": ("input_ids", "token_type_ids", "attention_mask"),
+    "bart": (
+        "input_ids",
+        "attention_mask",
+        "decoder_input_ids",
+        "decoder_attention_mask",
+    ),
+    "t5": (
+        "input_ids",
+        "attention_mask",
+        "decoder_input_ids",
+        "decoder_attention_mask",
+    ),
+}
+
+
 class BaseProcessor(ABC):
     def __init__(self, model_name_or_path: str, max_length: int) -> None:
         self._tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
