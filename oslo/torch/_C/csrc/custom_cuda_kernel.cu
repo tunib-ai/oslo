7002a42fa01b21f164a14257a5a2e050fb7c35f9
yupjun <yaup22c@naver.com>
2022-07-27 11:10:54 +0900

    CUDA global info function added

--- oslo/torch/_C/csrc/custom_cuda_kernel.cu
+++ oslo/torch/_C/csrc/custom_cuda_kernel.cu
@@ -1,4 +1,50 @@
 #include "custom_cuda_layers.h"
+#include <stdio.h>
+
+
+using namespace std;
+
+// to do list
+
+
+// 0. host and device split for torch _C binding
+// 0.0 CUDA side error casting
+// 0.1 CUDA 
+// 1. Single GPU basd grid,thread,WARP setting
+// 2. Selected Single GPU based,grid,thread,WARP setting
+// 3. Multi GPU based grid,thread,WARP setting
+// 4. selected Multi GPU based grid,thread,WARP setting
+
+
+// 0727 kernel ma
+__global__ std::vector<int> kernel_query(int deviceid)
+{
+  cudaGetDevice(&deviceId)
+  cudaDeviceProp props;
+  cudaGetDeviceProperties(&props, deviceId);
+  std::vector<int> current_cuda_props;
+  current_cuda_props.pushback(props.major);
+  current_cuda_props.pushback(props.minor);
+  current_cuda_props.pushback(props.multiProcessorCount);
+  current_cuda_props.pushback.(props.warpSize);
+  current_cuda_props.pushback.(props.maxThreadsPerBlock);
+  current_cuda_props.pushback.(props.maxThreadsDim[0]);
+  current_cuda_props.pushback.(props.maxThreadsDim[1]);
+  current_cuda_props.pushback.(props.maxThreadsDim[2]);
+  current_cuda_props.pushback.(props.maxGridSize[0]);
+  current_cuda_props.pushback.(props.maxGridSize[1]);
+  current_cuda_props.pushback.(props.maxGridSize[2]);
+
+  // printf("Device ID: %d\nNumber of SMs: 
+  // %d\nCompute Capability Major: %d\n
+  // Compute Capability Minor: %d\n
+  // Warp Size: %d\n"
+  // , deviceId, multiProcessorCount, computeCapabilityMajor, computeCapabilityMinor, warpSize);
+
+
+  return current_cuda_props;
+}
+
 
 __global__ void param_update_kernel(const float *input, __half *output,
                                     int size) {
