50ada74181a1ab1845169c68279f11728ac73ab9
gimmaru <john8778@naver.com>
2022-08-03 23:52:04 +0900

    update test electra

--- tests/transformers/models/electra/test_mlm.py
+++ tests/transformers/models/electra/test_mlm.py
@@ -1,11 +1,12 @@
-from oslo.transformers.tasks.data_roberta_pretraining import (
-    ProcessorForRobertaPretraining,
-    DataCollatorForRobertaPretraining,
+from oslo.transformers.tasks.data_masked_lm import (
+    ProcessorForMaskedLM,
+    DataCollatorForMaskedLM,
 )
 from oslo.transformers.models.electra.modeling_electra import ElectraForMaskedLM
 from datasets import load_dataset
 import os
 import wandb
+from torch.utils.data import DataLoader
 
 try:
     from transformers.models.electra.modeling_electra import (
@@ -23,13 +24,13 @@ def train(model, dataset, processor, data_collator, run_name, fp16):
     args = TrainingArguments(
         output_dir="output",
         evaluation_strategy="steps",
-        eval_steps=100,
+        eval_steps=40,
         per_device_train_batch_size=4,
         per_device_eval_batch_size=4,
         num_train_epochs=1,
         seed=0,
         load_best_model_at_end=True,
-        save_steps=100000,
+        save_steps=400000,
         fp16=fp16,
         run_name=run_name,
     )
@@ -54,19 +55,19 @@ if __name__ == "__main__":
     dataset = dataset.rename_column("sentence", "text")
 
     model_name_or_path = "google/electra-small-generator"
-    processor = ProcessorForRobertaPretraining(model_name_or_path)
+    processor = ProcessorForMaskedLM(model_name_or_path)
 
     processed_dataset = dataset.map(
         processor, batched=True, remove_columns=dataset["train"].column_names
     )
     processed_dataset.cleanup_cache_files()
 
+    data_collator = DataCollatorForMaskedLM(processor)
+
     oslo_model = ElectraForMaskedLM.from_pretrained(model_name_or_path)
     transformers_model = TransformersElectraForMaskedLM.from_pretrained(
         model_name_or_path
     )
-
-    data_collator = DataCollatorForRobertaPretraining(processor)
 
     # Transformers
     train(
@@ -87,7 +88,7 @@ if __name__ == "__main__":
         fp16=True,
     )
 
-    # Oslo
+    # # Oslo
     train(
         model=oslo_model,
         dataset=processed_dataset,
